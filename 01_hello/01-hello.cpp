ce8eba0ee6ebb9a775f7cbcc061288f931b86e08
xiransong <coastline1999@163.com>
2020-11-24 09:25:22 +0800

    my first window

---
+++ 01_hello/01-hello.cpp
@@ -0,0 +1,68 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	//创建窗口对象
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);  //将我们窗口的上下文设置为当前线程的主上下文
+
+	//GLAD是用来管理OpenGL的函数指针的
+	//初始化GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))  //给GLAD传入用来加载系统相关的OpenGL函数指针地址的函数
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);  //告诉OpenGL渲染窗口的尺寸大小
+
+	//注册这个回调函数，告诉GLFW我们希望每当窗口调整大小的时候调用这个函数：
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//渲染循环(Render Loop)，它能在我们让GLFW退出前一直保持运行
+	while (!glfwWindowShouldClose(window))
+	{
+		//输入
+		processInput(window);  //检查用户是否按下了返回键(Esc)
+
+		//渲染指令
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);  //我们使用一个自定义的颜色清空屏幕
+		glClear(GL_COLOR_BUFFER_BIT);
+		//glClearColor函数是一个状态设置函数，而glClear函数则是一个状态使用的函数
+
+		//检查并调用事件，交换缓冲
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();  //当渲染循环结束后我们需要正确释放/删除之前的分配的所有资源
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)  //检查用户是否按下了返回键(Esc)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
